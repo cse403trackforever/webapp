048162eaf1bc3ac4264d8dbd2bbbb9fe52a8fbb5
David Dupre <djdupre@uw.edu>
2018-04-12 17:56:14 -0700

    Add JSON file structures

---
+++ src/app/comment.ts
@@ -0,0 +1,4 @@
+export interface Comment {
+  commenterName: String;
+  content: String;
+}
---
+++ src/app/issue.ts
@@ -0,0 +1,15 @@
+import {Comment} from './comment';
+
+export interface Issue {
+  id: String;
+  projectId: String;
+  status: String;
+  summary: String;
+  labels: String[];
+  comments: Comment[];
+  submitterName: String;
+  assignees: String[];
+  timeCreated: Number;
+  timeUpdated: Number;
+  timeClosed: Number;
+}
