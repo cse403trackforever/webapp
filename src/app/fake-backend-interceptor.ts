6ce55b1a55ffd5f99ae26811eed94ba5d07f7d6f
David Dupre <djdupre@uw.edu>
2018-04-22 16:50:02 -0700

    Fix fake backend interceptor to pass through to GitHub API

--- src/app/fake-backend-interceptor.ts
+++ src/app/fake-backend-interceptor.ts
@@ -10,6 +10,7 @@ import 'rxjs/add/operator/dematerialize';
 import { mockIssue } from './shared/models/mock/mock-issue';
 import { mockProjectSummary } from './shared/models/mock/mock-project-summary';
 import { mockProject } from './shared/models/mock/mock-project';
+import { environment } from '../environments/environment';
 
 /**
  * This interceptor lets us mock the backend while it's still in development.
@@ -21,28 +22,30 @@ export class FakeBackendInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return Observable.of(null).mergeMap(() => {
-      // mock getIssues
-      if (request.url.endsWith('/issues')) {
-        return Observable.of(new HttpResponse({
-          status: 200,
-          body: mockIssue
-        }));
-      }
+      if (request.url.startsWith(environment.apiUrl)) {
+        // mock getIssues
+        if (request.url.endsWith('/issues')) {
+          return Observable.of(new HttpResponse({
+            status: 200,
+            body: mockIssue
+          }));
+        }
 
-      // mock getProjects
-      if (request.url.endsWith('/projects')) {
-        return Observable.of(new HttpResponse({
-          status: 200,
-          body: [mockProjectSummary]
-        }));
-      }
+        // mock getProjects
+        if (request.url.endsWith('/projects')) {
+          return Observable.of(new HttpResponse({
+            status: 200,
+            body: [mockProjectSummary]
+          }));
+        }
 
-      // mock getProject
-      if (request.url.match(/\/projects\/[^\/]*$/)) {
-        return Observable.of(new HttpResponse({
-          status: 200,
-          body: mockProject
-        }));
+        // mock getProject
+        if (request.url.match(/\/projects\/[^\/]*$/)) {
+          return Observable.of(new HttpResponse({
+            status: 200,
+            body: mockProject
+          }));
+        }
       }
 
       // pass through
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,5 +6,5 @@
 export const environment = {
   production: false,
   apiUrl: 'localhost:8080/trackforever', // TODO replace with local API
-  mockBackend: false,
+  mockBackend: true,
 };
